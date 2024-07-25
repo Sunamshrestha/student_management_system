c02e945c0540f252fa5b3fb2093255c958b42dc7
Sunamshrestha <Sunamshrestha@users.noreply.github.com>
2024-07-25 17:45:16 +0545

    third commit

--- realstudentapp/views.py
+++ realstudentapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render # type: ignore
 
 
 # Create your views here.
@@ -7,3 +9,9 @@ def showDemoPage(request):
 
 def ShowLoginPage(request):
     return render(request,"login_page.html")
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+       return HttpResponse("Email :"+request.POST.get("email")+"Password:"+request.POST.get("password")) 
