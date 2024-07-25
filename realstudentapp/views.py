8db6545aa5b89cf8b7ff3e150cd17907c1f60ef8
Sunamshrestha <Sunamshrestha@users.noreply.github.com>
2024-07-25 21:41:19 +0545

    fouth

--- realstudentapp/views.py
+++ realstudentapp/views.py
@@ -1,17 +1,32 @@
-from django.http import HttpResponse
-
-from django.shortcuts import render # type: ignore
-
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from realstudentapp.EmailBackEnd import EmailBackEnd
 
 # Create your views here.
 def showDemoPage(request):
-    return render(request,"demo.html")
-
+    return render(request,'demo.html')
 def ShowLoginPage(request):
-    return render(request,"login_page.html")
-
+    return render(request,'login_page.html')
 def doLogin(request):
     if request.method!="POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
+    else: 
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user!=None:
+            login(request,user)
+            return HttpResponse("Email:"+request.POST.get("email")+" Password: "+request.POST.get("password"))
+        else:
+
+            return HttpResponse("Invalid Login")
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User: "+request.user.email+" usertype: "+request.user.user_type)
     else:
-       return HttpResponse("Email :"+request.POST.get("email")+"Password:"+request.POST.get("password")) 
+        return HttpResponse("Please Login First")
+    
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
+
