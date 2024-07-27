cc0080e0ac9f5c2a5991be65b15f9679e367c9b7
Sunamshrestha <Sunamshrestha@users.noreply.github.com>
2024-07-27 12:10:19 +0545

    fourth-1

--- realstudentapp/views.py
+++ realstudentapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate,login,logout
@@ -15,10 +16,10 @@ def doLogin(request):
         user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
             login(request,user)
-            return HttpResponse("Email:"+request.POST.get("email")+" Password: "+request.POST.get("password"))
+            return HttpResponseRedirect('/admin_home')
         else:
-
-            return HttpResponse("Invalid Login")
+            messages.error(request,"Invalid LogIn details")
+            return HttpResponseRedirect("/")
 
 def GetUserDetails(request):
     if request.user!=None:
