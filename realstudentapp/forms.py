e412f0272777540cd2f39f04ca28b6b31fd02198
Sunamshrestha <Sunamshrestha@users.noreply.github.com>
2024-08-22 15:38:29 +0545

    chdjfk

--- realstudentapp/forms.py
+++ realstudentapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from realstudentapp.models import Courses
+from realstudentapp.models import Courses, SessionYearModel
 
 class DateInput(forms.DateInput):
     input_type = "date"
@@ -14,23 +14,32 @@ class AddStudentForm(forms.Form):
     last_name = forms.CharField(label = "Last Name", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     username = forms.CharField(label = "Usernam", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label = "Address", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
-
-    courses = Courses.objects.all()
     course_list = []
+    
+    courses = Courses.objects.all()
+
     for course in courses:
         small_course = (course.id,course.course_name)
         course_list.append(small_course)
+    
+    # course_list = []
+
+    session_list = []
+    sessions = SessionYearModel.object.all()
+
+    for ses in sessions:
+        small_ses = (ses.id,str(ses.session_start_year)+"  To  "+str(ses.session_end_year))
+        session_list.append(small_ses)
+   
 
     gender_choice = (
         ('Male',"Male"),
         ('Female',"Female"),
-
     ) 
 
     course = forms.ChoiceField(label = "Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}) )
     sex = forms.ChoiceField(label = "Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
-    session_start = forms.DateField(label = "Session Start",widget = DateInput(attrs={"class":"form-control"}))
-    session_end = forms.DateField(label = "Session End",widget= DateInput(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label = "Session Year",widget = forms.Select(attrs={"class":"form-control"}),choices=session_list)
     profile_pic = forms.FileField(label = "Profile Pic", max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}))
 
 class EditStudentForm(forms.Form):
@@ -40,11 +49,29 @@ class EditStudentForm(forms.Form):
     username = forms.CharField(label = "Usernam", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label = "Address", max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    courses = Courses.objects.all()
     course_list = []
-    for course in courses:
-        small_course = (course.id,course.course_name)
-        course_list.append(small_course)
+    try:
+        courses = Courses.objects.all()
+        for course in courses:
+            small_course = (course.id,course.course_name)
+            course_list.append(small_course)
+
+    except:
+        course_list=[]
+    
+        
+    session_list = []
+    try:
+
+
+        sessions = SessionYearModel.object.all()
+
+        for ses in sessions:
+            small_ses = (ses.id,str(ses.session_start_year)+"  To  "+str(ses.session_end_year))
+            session_list.append(small_ses)
+
+    except:
+        pass
 
     gender_choice = (
         ('Male',"Male"),
