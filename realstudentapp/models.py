94b119c65fa13c2ca878a006df908da5acfd548b
Sunamshrestha <Sunamshrestha@users.noreply.github.com>
2024-09-21 22:07:46 +0545

    jkfjs

--- realstudentapp/models.py
+++ realstudentapp/models.py
@@ -28,6 +28,7 @@ class Staffs(models.Model):
     address=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+    fcm_token=models.TextField(default="")
     objects=models.Manager()
 
 class Courses(models.Model):
@@ -56,14 +57,15 @@ class Students(models.Model):
     session_year_id = models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+    fcm_token=models.TextField(default="")
     objects = models.Manager()
 
 class Attendance(models.Model):
     id=models.AutoField(primary_key=True)
     subject_id=models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
-    attendance_date=models.DateTimeField(auto_now_add=True)
-    session_year_id = models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
+    attendance_date=models.DateField()
     created_at=models.DateTimeField(auto_now_add=True)
+    session_year_id = models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
@@ -91,7 +93,7 @@ class LeaveReportStaff(models.Model):
     staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
     leave_date = models.CharField(max_length=255)
     leave_message = models.TextField()
-    leave_status = models.BooleanField(default=False)
+    leave_status = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
@@ -125,7 +127,6 @@ class NotificationStudent(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
-
 class NotificationStaffs(models.Model):
     id = models.AutoField(primary_key=True)
     staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
@@ -133,6 +134,32 @@ class NotificationStaffs(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+class StudentResult(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.CASCADE)
+    subject_exam_marks=models.FloatField(default=0)
+    subject_assignment_marks=models.FloatField(default=0)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now_add=True)
+    objects=models.Manager()
+
+
+class OnlineClassRoom(models.Model):
+    id=models.AutoField(primary_key=True)
+    room_name=models.CharField(max_length=255)
+    room_pwd=models.CharField(max_length=255)
+    subject=models.ForeignKey(Subjects,on_delete=models.CASCADE)
+    session_years=models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
+    started_by=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    is_active=models.BooleanField(default=True)
+    created_on=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+
+
+
 
 @receiver(post_save,sender=CustomUser)
 def create_user_profile(sender,instance,created,**kwargs):
@@ -142,7 +169,7 @@ def create_user_profile(sender,instance,created,**kwargs):
         if instance.user_type==2:
             Staffs.objects.create(admin=instance,address="")
         if instance.user_type==3:
-            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_year_id=SessionYearModel.object.get(id=1),address="",profile_pic="",gender="")
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_year_id=SessionYearModel.object.get(id=1),address="",profile_pic="",gender="")       
 
 @receiver(post_save,sender=CustomUser)
 def save_user_profile(sender,instance,**kwargs):
